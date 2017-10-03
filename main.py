3519207a0529f22430d034d59309c1afcfe95fd5
gumpena_ravi <gmpn1996@gmail.com>
2017-10-03 15:11:34 +0530

    Added all the scores and username

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
-def main():
-	number_list = [0,1,2,4,6]
+def main_game():
+	number_list = [0,1,2,3,4,5,6]
 	total_score = 0
 	total_balls = 0
 	user_input = None
@@ -25,22 +25,27 @@ def main():
 				if user_input == 6 or user_input == 4 :
 					print("Empire says great work 'Its a "+str(user_input))
 				total_score = total_score + user_input
-			total_balls = total_balls + 1
+			if gen_num != 0 :
+				total_balls = total_balls + 1
+			else :
+				print("Oops No ball")
 
 		else:
-			print("Choose number in [0,1,2,4,6]")
+			print("Choose number in range [0,6]")
 			continue
 	return total_balls,total_score
 
-def display_score(total_balls,total_score):
+def display_score(total_balls,total_score,player_name):
 	print("\n*********************************************")
+	print("Well Played "+player_name+" , Here is your score")
 	print("Your total score is :\t"+str(total_score))
 	print("You Played "+ str(int(total_balls/6)) + " (Overs) and " \
 		+ str(int(total_balls%6)) + " (Balls)")
 	print("\n*********************************************")
 	
 if __name__ == '__main__':
-	total_balls,total_score = main()
-	display_score(total_balls,total_score)
-	print("\n Thank you for playing  'Hand Cricket' ")
+	total_balls,total_score = main_game()
+	player_name = input("Enter you name :")
+	display_score(total_balls,total_score,player_name)
+	print("\n Keep playing  'Hand Cricket' the only fun game")
 # print(user_input, gen_num)
